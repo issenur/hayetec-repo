f34b19c091cf36643c5ed402e118ad7e6acb832d
Isse Nur <issenur@gmail.com>
2017-11-03 05:49:06 +0100

    started Lap 8 sHP->CSE

---
+++ RunnyStack.java
@@ -0,0 +1,174 @@
+class RunnyStack<Base>
+{
+  /*
+
+  . Each instance of
+  Run represents a run of objects. You will receive no points for this
+  assignment if you use arrays in any way! The class Run must have three
+  private slots that have the following names and types. The slot object
+  points to the Base object that appears in the run. The slot length is an
+  int that is the length of the run. The slot next points to the instance of
+   Run that is immediately below this one on the stack, or to null. It must
+   also have a private constructor that initializes these slots.
+
+    Third, your push method must test non-null objects for equality using their equals methods. It must use the Java ‘==’ operator only for testing null objects. It is helpful to define an extra private method called isEqual that takes two objects as arguments, and tests if they are equal. If either object is null, then isEqual uses ‘==’. If neither object is null, then isEqual uses equals.
+
+    Fourth, RunnyStack’s methods are not allowed to print things. If you were writing RunnyStack in the Real World, then it might be part of some larger program. You don’t know if that larger program should print things.*/
+  private class Run
+  {
+    private Base object;
+    private Run next;
+    private int length;
+    private Run(Base object, Run next, int length)
+    {
+      this.object = object;
+      this.next = next;
+      this.length = length;
+    }
+  }
+  public RunnyStack()
+  {
+
+  }
+
+  public int depth()
+  {
+
+  }
+
+  public boolean isEmpty()
+  {
+
+  }
+
+  public Base peek()
+  {
+
+  }
+
+  public void pop()
+  {
+
+  }
+
+  public void push(Base object)
+  {
+
+  }
+
+  public int runs()
+  {
+
+  }
+}
+//
+//  Tests for CSci 1913 Lab 8
+//  James Moen
+//  20 Mar 17
+//
+//  The TRY-CATCH statements catch exceptions thrown by RUNNY STACK's methods,
+//  so that the program can continue to run even if a method fails. We still
+//  haven't talked about TRY-CATCH'es in the lectures yet.
+//
+//  Most tests have comments that show what they should print, and how many
+//  points they are worth, for a total of 40 points.
+//
+//  Camembert is a soft French cheese. It may be runny. It can be stacked.
+//
+
+class Camembert
+{
+  public static void main(String [] args)
+  {
+    RunnyStack<String> s = new RunnyStack<String>();
+
+    System.out.println(s.isEmpty());         //  true       1 point
+    System.out.println(s.depth());           //  0          1 point
+    System.out.println(s.runs());            //  0          1 point
+
+    try
+    {
+      s.pop();
+    }
+    catch (IllegalStateException ignore)
+    {
+      System.out.println("No pop");          //  No pop     1 point
+    }
+
+    try
+    {
+      System.out.println(s.peek());
+    }
+    catch (IllegalStateException ignore)
+    {
+      System.out.println("No peek");         //  No peek    1 point
+    }
+
+    s.push("A");
+    System.out.println(s.peek());            //  A          1 point
+    System.out.println(s.depth());           //  1          1 point
+    System.out.println(s.runs());            //  1          1 point
+
+    System.out.println(s.isEmpty());         //  false      1 point
+
+    s.push("B");
+    System.out.println(s.peek());            //  B          1 point
+    System.out.println(s.depth());           //  2          1 point
+    System.out.println(s.runs());            //  2          1 point
+
+    s.push("B");
+    System.out.println(s.peek());            //  B          1 point
+    System.out.println(s.depth());           //  3          1 point
+    System.out.println(s.runs());            //  2          1 point
+
+    s.push("B");
+    System.out.println(s.peek());            //  B          1 point
+    System.out.println(s.depth());           //  4          1 point
+    System.out.println(s.runs());            //  2          1 point
+
+    s.push("C");
+    System.out.println(s.peek());            //  C          1 point
+    System.out.println(s.depth());           //  5          1 point
+    System.out.println(s.runs());            //  3          1 point
+
+    s.push("C");
+    System.out.println(s.peek());            //  C          1 point
+    System.out.println(s.depth());           //  6          1 point
+    System.out.println(s.runs());            //  3          1 point
+
+    s.pop();
+    System.out.println(s.peek());            //  C          1 point
+    System.out.println(s.depth());           //  5          1 point
+    System.out.println(s.runs());            //  3          1 point
+
+    s.pop();
+    System.out.println(s.peek());            //  B          1 point
+    System.out.println(s.depth());           //  4          1 point
+    System.out.println(s.runs());            //  2          1 point
+
+    s.pop();
+    System.out.println(s.peek());            //  B          1 point
+    System.out.println(s.depth());           //  3          1 point
+    System.out.println(s.runs());            //  2          1 point
+
+    s.pop();
+    s.pop();
+    System.out.println(s.peek());            //  A          1 point
+    System.out.println(s.depth());           //  1          1 point
+    System.out.println(s.runs());            //  1          1 point
+
+    s.pop();
+    System.out.println(s.isEmpty());         //  true       1 point
+    System.out.println(s.depth());           //  0          1 point
+    System.out.println(s.runs());            //  0          1 point
+
+    try
+    {
+      System.out.println(s.peek());
+    }
+    catch (IllegalStateException ignore)
+    {
+      System.out.println("No peek");         //  No peek    1 point
+    }
+  }
+}
