f1b69b0d3a852c4a177cbf69fd0f1cbef67ba26e
Isse Nur <nurxx050@csel-kh4240-10.cselabs.umn.edu>
2017-11-20 18:33:33 -0600

    improved and running project2

--- Project2_nurxx050.java
+++ Project2_nurxx050.java
@@ -5,12 +5,19 @@ class Poly
     private int coef;
     private int expo;
     private Term next;
-  }
 
+    public Term(int coef, int expo, Term next)
+    {
+      this.coef = coef;
+      this.expo = expo;
+      this.next = next;
+    }
+  }
+  Term head;
 
   public Poly()
   {
-
+    head = new Term(5,5,null);
   }
   /* */
   public Poly term(int coef, int expo)
@@ -42,7 +49,7 @@ class Poly
           Term immediately before that Term,*/
           left.next = new Term(coef, expo, right);
           /*and stop visiting Term’s.*/
-          break;
+          return this;
         }
         /*If you visit a Term whose expo slot
         is greater than the parameter expo*/
@@ -60,7 +67,7 @@ class Poly
           /*then throw an IllegalArgumentException.*/
           throw new IllegalArgumentException();
         }
-        else if(right.next = null)
+        else if(right.next == null)
         {
           /*If you’ve visited all the Term’s, but didn’t add a
           new Term, then add the new Term at the end of the
@@ -69,11 +76,17 @@ class Poly
         }
       /*Finally, return this so that more Term’s
       can be added later*/
-      return this;
+
       }
+      if(right == null)
+      {
+        left.next = new Term(coef, expo, right);
+      }
+    }
+    return this;
   }
 
-  public Poly plus(Poly that)
+  /*public Poly plus(Poly that)
   {
 
   }
@@ -86,10 +99,43 @@ class Poly
   public Poly minus()
   {
 
-  }
+  }*/
 
   public String toString()
   {
+    StringBuilder builder = new StringBuilder();
+    Term temp = head.next;
 
+    while(temp != null)
+    {
+      builder.append(temp.coef);
+      builder.append("x");
+      builder.append(temp.expo);
+      if(temp.next!= null)
+      {
+        builder.append(" + ");
+      }
+      temp = temp.next;
+    }
+    return builder.toString();
+  }
+}
+
+class PollyEsther
+{
+  public static void main(String[] args)
+  {
+    //Poly p0 = new Poly();
+    Poly p1 = new Poly().term(1, 3).term(1, 1).term(1, 2);
+    Poly p2 = new Poly().term(2, 0);
+    //Poly p3 = p2.minus();
+
+    //System.out.println(p0);           //  0
+    //System.out.println(p1);           //  1x3 + 1x2 + 1x1
+    System.out.println(p2);           //  3x2 + 2x1
+    //System.out.println(p3);           //  −3x2 − 2x1
+
+    //System.out.println(p1.plus(p2));  //  1x3 + 4x2 + 3x1
+    //System.out.println(p1.plus(p3));  //  1x3 − 2x2 − 1x1
   }
 }
